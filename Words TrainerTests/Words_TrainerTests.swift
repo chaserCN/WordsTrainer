1fc38c16eecb2786554366c9ba1ef498a994b4f5
Nikolay Popok <nikolay.p@uniweb.dev>
2026-05-28 22:07:16 +0300

    Initial Commit

---
+++ Words TrainerTests/Words_TrainerTests.swift
@@ -0,0 +1,17 @@
+//
+//  Words_TrainerTests.swift
+//  Words TrainerTests
+//
+//  Created by Nikolay on 28.05.2026.
+//
+
+import Testing
+@testable import Words_Trainer
+
+struct Words_TrainerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
